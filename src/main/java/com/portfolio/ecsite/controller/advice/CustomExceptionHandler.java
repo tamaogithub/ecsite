34364f3dccca9d66d33c60f166a94f7265acd9d0
akira tamao <farnox3@gmail.com>
2023-01-12 23:29:03 +0900

    404 Bad Request:ボディを返すようにハンドラーを変更しよう

--- src/main/java/com/portfolio/ecsite/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/portfolio/ecsite/controller/advice/CustomExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.portfolio.ecsite.controller.advice;
 
+import com.portfolio.ecsite.model.ResourceNotFoundError;
 import com.portfolio.ecsite.service.item.ItemEntityNotFoundException;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(ItemEntityNotFoundException.class)
-    public ResponseEntity<Void> handleTaskEntityNotFoundException(ItemEntityNotFoundException e){
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(ItemEntityNotFoundException e){
+        var error = new ResourceNotFoundError();
+        error.setDetail(e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
     }
 }

de3d5f33e4b256cab1bc6166b294ae9dad340a80
akira tamao <farnox3@gmail.com>
2023-01-15 22:59:39 +0900

    POST Controller 400 Bad Requestのレスポンスボディを作成する

--- src/main/java/com/portfolio/ecsite/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/portfolio/ecsite/controller/advice/CustomExceptionHandler.java
@@ -1,17 +1,23 @@
 package com.portfolio.ecsite.controller.advice;
 
+import com.portfolio.ecsite.model.BadRequestError;
 import com.portfolio.ecsite.model.ResourceNotFoundError;
 import com.portfolio.ecsite.service.item.ItemEntityNotFoundException;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
 
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ItemEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(ItemEntityNotFoundException e){
@@ -21,5 +27,15 @@ public class CustomExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(new BadRequestError());
     }
 }

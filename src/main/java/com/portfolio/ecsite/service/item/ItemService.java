eb8e43fad7c8d362437c304abc5f0ec1506850cf
akira tamao <farnox3@gmail.com>
2023-01-15 15:29:36 +0900

    POST Service requestBody を定義

--- src/main/java/com/portfolio/ecsite/service/item/ItemService.java
+++ src/main/java/com/portfolio/ecsite/service/item/ItemService.java
@@ -12,7 +12,14 @@ public class ItemService {
     public ItemEntity find(Long itemId) {
         // レコードが1件も取得できない事も考慮にいれて Optional を返すようにする
         return itemRepository.select(itemId)
-                .map(record -> new ItemEntity(record.getId(), record.getTitle()))
+                .map(record -> new ItemEntity(record.getId(), record.getItemName()))
                 .orElseThrow(() -> new ItemEntityNotFoundException(itemId));
+    }
+
+    public ItemEntity create(String itemName) {
+        return new ItemEntity(999L,itemName);
+//        var record = new ItemRecord(null,itemName);
+//        itemRepository.insert(record);
+//        return new ItemEntity(record.getId(), record.getItemName());
     }
 }

3aee4fbd7033f07536bcc74f1975d7cd4bcbdc73
akira tamao <farnox3@gmail.com>
2023-02-02 18:40:42 +0900

    ItemService 修正

--- src/main/java/com/portfolio/ecsite/service/item/ItemService.java
+++ src/main/java/com/portfolio/ecsite/service/item/ItemService.java
@@ -4,22 +4,69 @@ import com.portfolio.ecsite.repository.item.ItemRecord;
 import com.portfolio.ecsite.repository.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
-
     private final ItemRepository itemRepository;
+
     public ItemEntity find(Long itemId) {
         // レコードが1件も取得できない事も考慮にいれて Optional を返すようにする
         return itemRepository.select(itemId)
-                .map(record -> new ItemEntity(record.getId(), record.getItemName()))
+                .map(record -> new ItemEntity(
+                        record.getId(),
+                        record.getItemName(),
+                        record.getDescription(),
+                        record.getItemImage(),
+                        record.getCompany(),
+                        record.getPrice(),
+                        record.getStock()))
                 .orElseThrow(() -> new ItemEntityNotFoundException(itemId));
     }
 
-    public ItemEntity create(String itemName) {
-        var record = new ItemRecord(null,itemName);
-        itemRepository.insert(record);
-        return new ItemEntity(record.getId(), record.getItemName());
+    //    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<ItemEntity> findAll(int limit ,long offset) {
+        //取得したList<itemRecord>をstream()でItemEntityに変換し、最後にList<ItemEntity>に変換する
+        return itemRepository.selectList(limit, offset)
+                .stream()
+                .map(record -> new ItemEntity(
+                        record.getId(),
+                        record.getItemName(),
+                        record.getDescription(),
+                        record.getItemImage(),
+                        record.getCompany(),
+                        record.getPrice(),
+                        record.getPrice()))
+                .collect(Collectors.toList());
     }
+
+    public int findTotal() {
+        return itemRepository.selectListCount();
+    }
+
+
+    @Transactional
+    public ItemEntity create(String itemName, String  description, byte[] itemImage, String company, int price, int stock) {
+        var record = new ItemRecord(null,itemName, description, itemImage ,company, price, stock);
+        itemRepository.insert(record);
+
+        return new ItemEntity(
+                record.getId(),
+                record.getItemName(),
+                record.getDescription(),
+                record.getItemImage(),
+                record.getCompany(),
+                record.getPrice(),
+                record.getStock());
+    }
+
+    @Transactional
+    public void update(Long itemId, String itemName, String description, byte[] itemImage, String company, int price, int stock) {
+        itemRepository.update(itemId, itemName, description, itemImage, company, price, stock);
+    }
+
 }

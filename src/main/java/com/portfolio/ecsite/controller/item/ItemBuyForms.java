4ce8947c68feb316d0c153ea0e9eaae78888b75c
akira tamao <farnox3@gmail.com>
2023-02-19 22:24:18 +0900

    商品購入のフィールド定義

---
+++ src/main/java/com/portfolio/ecsite/controller/item/ItemBuyForms.java
@@ -0,0 +1,11 @@
+package com.portfolio.ecsite.controller.item;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class ItemBuyForms {
+
+    private int stock;
+}

d2046c547f8aca959f8a386540b2bdb0e02c6568
akira tamao <farnox3@gmail.com>
2023-02-21 20:50:07 +0900

    支払い方法のMapオブジェクト 追加

--- src/main/java/com/portfolio/ecsite/controller/item/ItemBuyForms.java
+++ src/main/java/com/portfolio/ecsite/controller/item/ItemBuyForms.java
@@ -2,11 +2,24 @@ package com.portfolio.ecsite.controller.item;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Data
 @RequiredArgsConstructor
 public class ItemBuyForms {
 
     private int stock;
+    private String address;
     private String payment;
+
+    /** 支払い方法のMapオブジェクト */
+    public Map<String,String> getPaymentItems(){
+        Map<String, String> paymentMap = new LinkedHashMap<String, String>();
+        paymentMap.put("1", "請求書払い");
+        paymentMap.put("2", "口座振替");
+        paymentMap.put("3", "クレジットカード");
+        return paymentMap;
+    }
+
 }

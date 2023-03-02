f76e5f885bdf63a0165d087c6458d5e7cc793ef3
akira tamao <farnox3@gmail.com>
2023-03-02 11:14:41 +0900

    javadoc 値 修正

--- src/main/java/com/portfolio/ecsite/common/FileOpeUtil.java
+++ src/main/java/com/portfolio/ecsite/common/FileOpeUtil.java
@@ -2,10 +2,7 @@ package com.portfolio.ecsite.common;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Base64;
@@ -19,65 +16,28 @@ public class FileOpeUtil {
 
     private static String base64Data;
 
-    /**
-     * アップロード実行処理
-     * @param multipartFile
-     * @return bytes
-     */
-    static public byte[] uploadAction(MultipartFile multipartFile) throws IOException {
-        //ファイル名取得
-        String fileName = multipartFile.getOriginalFilename();
-        //アップロードファイルをバイト値に変換
-        byte[] bytes = multipartFile.getBytes();
-        return bytes;
-    }
 
     /**
-     * ファイルが画像なのか判定
+     * ファイル操作用のユーティリティクラス
      * @param file
-     * @retun ture : ファイル画像 , false: ファイル画像意外
+     * @return bytes
+     * @throws IOException
      */
-    static public boolean isImageFileByImageIO(MultipartFile file) {
+    static public byte[] uploadAction(MultipartFile file) throws IOException {
 
-        //MultipartFileのfileName を取得
-        String fileName = file.getOriginalFilename();
-
-        try {
-            // MultipartFileをFile形式に変換
-            File convFile = new File(fileName);
-
-            // ImageIOで、ファイルを読み込み
-            if (convFile != null && convFile.isFile()) {
-                // 拡張子を除いたファイル名を取得
-                convFile.createNewFile();
-                FileOutputStream fos = new FileOutputStream(convFile);
-                fos.write(file.getBytes());
-                fos.close();
-                BufferedImage bi = ImageIO.read(convFile);
-
-                // 引数に渡したFileが画像ファイル以外の場合、BufferedImageがnullで返ってくる。
-                if (bi != null) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException("Error uploading file.", e);
-        }
+        //アップロードファイルをバイト値に変換
+        byte[] bytes = file.getBytes();
+        return bytes;
     }
 
     /**
      * アップロードされたファイルをbase64ビッドに変換
      * @param image
-     * @retun base64Data
+     * @return base64Data
+     * @throws IOException
      */
     static public String fileToBase64Data(MultipartFile image) throws IOException {
         //画像ファイル名取得
-        String fileName = image.getOriginalFilename();
         byte[] bytes = uploadAction(image);
         String base64Data = Base64.getEncoder().encodeToString(bytes);
         return base64Data;
@@ -86,7 +46,7 @@ public class FileOpeUtil {
 
     /**
      * アップロードされたファイルをbase64ビッドに変換
-     * @retun base64Data
+     * @return base64Data
      */
     static public String noImageFileToBase64Data(){
         //画像ファイルの指定
@@ -102,7 +62,8 @@ public class FileOpeUtil {
 
     /**
      * アップロードされたファイルの拡張子を取得し画像かどうか判定する
-     * @retun
+     * @param image
+     * @return bool値
      */
     static public Boolean isImageMimeType(MultipartFile image) {
         String contentType = image.getContentType();

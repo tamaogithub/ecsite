73a297dfedd706ca04684393618afd92709e1829
akira tamao <farnox3@gmail.com>
2023-03-02 22:06:44 +0900

    変数の修正

--- src/test/java/com/portfolio/ecsite/validation/MediaTypeImageValidatorTest.java
+++ src/test/java/com/portfolio/ecsite/validation/MediaTypeImageValidatorTest.java
@@ -11,26 +11,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisplayName("MediaTypeImageValidatorのテスト")
 class MediaTypeImageValidatorTest {
 
-    private final MediaTypeImageValidator validator = new MediaTypeImageValidator();
+    private final MediaTypeImageValidator mediaTypeImageValidator = new MediaTypeImageValidator();
 
     @Test
     @DisplayName("画像ファイルが正しい形式である場合、trueを返す")
     void isValid_whenImageIsValid_returnsTrue() throws Exception {
         MockMultipartFile mockImage = new MockMultipartFile("image", "test.jpg", MediaType.IMAGE_JPEG_VALUE, "test".getBytes());
-        assertTrue(validator.isValid(mockImage, null));
+        assertTrue(mediaTypeImageValidator.isValid(mockImage, null));
     }
 
     @Test
     @DisplayName("画像ファイルが不正な形式である場合、falseを返す")
     void isValid_whenImageIsInvalid_returnsFalse() throws Exception {
         MockMultipartFile mockImage = new MockMultipartFile("image", "test.txt", MediaType.TEXT_PLAIN_VALUE, "test".getBytes());
-        assertFalse(validator.isValid(mockImage, null));
+        assertFalse(mediaTypeImageValidator.isValid(mockImage, null));
     }
 
     @Test
     @DisplayName("空の画像ファイルが渡された場合、trueを返す")
     void isValid_whenImageIsEmpty_returnsTrue() throws Exception {
         MockMultipartFile mockImage = new MockMultipartFile("image", "", null, new byte[0]);
-        assertTrue(validator.isValid(mockImage, null));
+        assertTrue(mediaTypeImageValidator.isValid(mockImage, null));
     }
 }

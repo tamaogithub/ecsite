9109bee1673b099735e37a6a11daf054db8e96ac
akira tamao <farnox3@gmail.com>
2023-03-04 16:36:09 +0900

    @DisplayName("ユーザー一覧画面のアクセス") パラメータの修正

--- src/test/java/com/portfolio/ecsite/controller/user/UserControllerTest.java
+++ src/test/java/com/portfolio/ecsite/controller/user/UserControllerTest.java
@@ -9,7 +9,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -36,16 +39,27 @@ class UserControllerTest {
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     class ControllerTest {
 
-        @Test @Order(1) @DisplayName("ユーザー一覧画面のアクセス")
+        @Test
+        @Order(1)
+        @DisplayName("ユーザー一覧画面のアクセス")
         void showList() throws Exception {
-            MvcResult result = mockMvc.perform(get("/users?limit=10&offset=0").accept(MediaType.TEXT_HTML))
-                    .andExpect(model().attributeExists("total", "page", "totalPage", "startPage", "endPage", "offset", "preOffset", "userList"))
-                    .andExpect(status().isOk())
-                    .andExpect(model().hasNoErrors())
-                    .andExpect(view().name("users/list")).andReturn();
+            // テスト用のパラメータを作成
+            HashMap<String, String> params = new HashMap<>();
+            params.put("limit", "10");
+            params.put("offset", "0");
+            MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/users")
+                    .param("limit", params.getOrDefault("limit", "10"))
+                    .param("offset", params.getOrDefault("offset", "0")))
+                .andExpect(model().attributeExists("total", "page", "totalPage", "startPage", "endPage", "offset", "preOffset", "userList"))
+                .andExpect(status().isOk())
+                .andExpect(model().hasNoErrors())
+                .andExpect(view().name("users/list")).andReturn();
         }
 
-        @Test @Order(2) @DisplayName("ユーザー登録画面のアクセス")
+        @Test
+        @Order(2)
+        @DisplayName("ユーザー登録画面のアクセス")
         void showCreationForm() throws Exception {
             MvcResult result = mockMvc.perform(get("/users/creationForm").accept(MediaType.TEXT_HTML))
                     .andExpect(status().isOk())
@@ -53,12 +67,16 @@ class UserControllerTest {
                     .andExpect(view().name("users/creationForm")).andReturn();
         }
 
-        @Test @Order(3) @DisplayName("登録ボタン押下後、ユーザ一覧画面にリダイレクトする")
+        @Test
+        @Order(3)
+        @DisplayName("登録ボタン押下後、ユーザ一覧画面にリダイレクトする")
         void createUser() {
         }
 
 
-        @Test @Order(4) @DisplayName("ユーザー編集画面のアクセス")
+        @Test
+        @Order(4)
+        @DisplayName("ユーザー編集画面のアクセス")
         void showUpdateFrom() throws Exception {
             mockMvc.perform(get("/users/update/1"))
                     .andExpect(status().isOk())
@@ -66,7 +84,9 @@ class UserControllerTest {
                     .andExpect(view().name("users/updateForm"));
         }
 
-        @Test @Order(5) @DisplayName("ユーザー編集エラー画面のアクセス")
+        @Test
+        @Order(5)
+        @DisplayName("ユーザー編集エラー画面のアクセス")
         void showUserUpdateErrorFrom() throws Exception {
             mockMvc.perform(get("/users/update//error/1"))
                     .andExpect(status().isOk())
@@ -74,7 +94,9 @@ class UserControllerTest {
                     .andExpect(view().name("users/updateErrorForm"));
         }
 
-        @Test @Order(6) @DisplayName("編集ボタン押下し、ユーザー一覧画面にリダイレクトする")
+        @Test
+        @Order(6)
+        @DisplayName("編集ボタン押下し、ユーザー一覧画面にリダイレクトする")
         void updateUser() {
         }
     }

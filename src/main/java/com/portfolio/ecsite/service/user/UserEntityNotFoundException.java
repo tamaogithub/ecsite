e112ca9750d3cc22511677df2a98918c3d7b247c
akira tamao <farnox3@gmail.com>
2023-03-05 16:35:19 +0900

    getUserId　追加

--- src/main/java/com/portfolio/ecsite/service/user/UserEntityNotFoundException.java
+++ src/main/java/com/portfolio/ecsite/service/user/UserEntityNotFoundException.java
@@ -2,10 +2,14 @@ package com.portfolio.ecsite.service.user;
 
 public class UserEntityNotFoundException extends RuntimeException{
 
-    private long userId;
+    private Long userId;
 
-    public UserEntityNotFoundException(long userId){
+    public UserEntityNotFoundException(Long userId){
         super("UserEntity ( id = " + userId + ") is not found.");
         this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
     }
 }

858493aa80cb06d5e5e35071cc03c14992c24ead
akira tamao <farnox3@gmail.com>
2023-03-06 12:51:10 +0900

    バリデーション追加

--- src/main/java/com/portfolio/ecsite/controller/item/ItemBuyForms.java
+++ src/main/java/com/portfolio/ecsite/controller/item/ItemBuyForms.java
@@ -1,5 +1,6 @@
 package com.portfolio.ecsite.controller.item;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -8,16 +9,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Data
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class ItemBuyForms {
 
-    @Min(value = 1, message = "{value}以上の値を設定してください")
-    @Max(value = 1000000000, message = "{value}未満の値を設定してください")
+    @Min(value = 1, message = "{value}以上で入力してください")
+    @Max(value = 1000000000, message = "{value}以下で入力してください")
     @NotNull(message = "空白は許可されていません")
     private Integer stock;
 
+    @Max(value = 1000000000, message = "{value}以下で入力してください")
+    private Integer quantity;
+
     @NotBlank
-    @Size(max=100)
+    @Size(max = 100, message = "100文字以下で入力してください")
     private String address;
 
     @NotNull(message = "空白は許可されていません")
@@ -31,5 +36,4 @@ public class ItemBuyForms {
         paymentMap.put("3", "クレジットカード");
         return paymentMap;
     }
-
 }

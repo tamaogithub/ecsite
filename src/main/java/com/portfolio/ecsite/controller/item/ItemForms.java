a3a899e0c09820a2e1f7c01ae534d3b07172dcd1
akira tamao <farnox3@gmail.com>
2023-03-13 18:49:19 +0900

    価格　バリデーション修正

--- src/main/java/com/portfolio/ecsite/controller/item/ItemForms.java
+++ src/main/java/com/portfolio/ecsite/controller/item/ItemForms.java
@@ -17,24 +17,25 @@ public class ItemForms {
     @Size(max = 20, message = "20文字以下で入力してください")
     private String itemName;
 
-    @NotBlank
-    @Size(max = 100, message = "100文字以下で入力してください")
-    private String description;
-
-    @Size(max = 255, message = "255文字以下で入力してください")
-    private String fileName;
-
     @MediaTypeImage
     private MultipartFile itemImage;
+
+    @NotBlank
+    @Size(max = 300, message = "300文字以下で入力してください")
+    private String description;
+
+    @Size(max = 100, message = "100文字以下で入力してください")
+    private String fileName;
 
     @NotBlank
     @Size(max = 100, message = "100文字以下で入力してください")
     private String company;
 
+    @NotBlank
+    @Pattern(regexp = "^\\d+$|^$", message = "数値を入力してください")
     @Min(value = 1, message = "{value}以上で入力してください")
     @Max(value = 1000000000, message = "{value}以下で入力してください")
-    @NotNull(message = "空白は許可されていません")
-    private Integer price;
+    private String price;
 
     @Min(value = 1, message = "{value}以上で入力してください")
     @Max(value = 1000000000, message = "{value}以下で入力してください")

0ade30dde219a8072feb0ed3671f51aa51aca6e8
akira tamao <farnox3@gmail.com>
2023-03-14 13:06:19 +0900

    price をInteger　⇒Stringに変換

--- src/main/java/com/portfolio/ecsite/repository/item/ItemRepository.java
+++ src/main/java/com/portfolio/ecsite/repository/item/ItemRepository.java
@@ -28,13 +28,16 @@ public interface ItemRepository {
 
     @Update("update items set itemname = #{itemName}, description = #{description}, filename = #{fileName}, " +
             "itemimage = #{itemImage}, company = #{company}, price = #{price}, stock = #{stock} where id = #{itemId}")
-    void update(Long itemId, String itemName, String description, String fileName, String itemImage ,String company, Integer price, Integer stock);
+    void update(Long itemId, String itemName, String description, String fileName, String itemImage ,String company, String price, Integer stock);
 
     @Update("update items set itemname = #{itemName}, description = #{description}, filename = #{fileName}, " +
             "company = #{company}, price = #{price}, stock = #{stock} where id = #{itemId}")
-    void updateExpectItemImage(Long itemId, String itemName, String description, String fileName, String company, Integer price, Integer stock);
+    void updateExpectItemImage(Long itemId, String itemName, String description, String fileName, String company, String price, Integer stock);
     @Update("update items set stock = #{stock}, payment = #{payment} where id = #{itemId}")
     void itemBuy(Long itemId, Integer stock, String payment);
+
+//    @Insert("insert into items (user_id, item_id, quantity) values (#{userId}, #{itemId}, #{quantity})")
+//    void orderInsert(Long userId, Long itemId, Integer quantity);
 
     @Select("SELECT stock FROM items where id = #{itemId}")
     int getStock(Long itemId);
